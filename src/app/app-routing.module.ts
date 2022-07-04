32b24f0f49867a8fd830ad963537e7b0fbff87dc
Marcus Blackhall <marcusinamsterdam@gmail.com>
2022-07-04 13:15:24 +0200

    Auth guard on servers component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from "@angular/core";
 import { RouterModule, Routes } from "@angular/router";
+import { AuthGuard } from "./auth-guard.service";
 import { HomeComponent } from "./home/home.component";
 import { PageNotFoundComponent } from "./page-not-found/page-not-found.component";
 import { EditServerComponent } from "./servers/edit-server/edit-server.component";
@@ -10,26 +11,32 @@ import { UsersComponent } from "./users/users.component";
 
 
 const appRoutes: Routes = [
-    { path: '', 'component': HomeComponent },
-    { path: 'users', 'component': UsersComponent,children: [
+  { path: '', 'component': HomeComponent },
+  {
+    path: 'users', 'component': UsersComponent, children: [
       { path: ':id/:name', 'component': UserComponent }
-    ] },
-   
-    {
-      path: 'servers', 'component': ServersComponent, children: [
-        { path: ':id/edit', 'component': EditServerComponent },
-        { path: ':id', 'component': ServerComponent }
-      ]
-    },
-  
-    {path: 'not-found', component: PageNotFoundComponent},
-    {path: '**', redirectTo: "/not-found"}
-  
-  ];
+    ]
+  },
+
+  {
+    path: 'servers',
+    // canActivate: [AuthGuard],
+    canActivateChild:[AuthGuard],
+    'component': ServersComponent,
+    children: [
+      { path: ':id/edit', 'component': EditServerComponent },
+      { path: ':id', 'component': ServerComponent }
+    ]
+  },
+
+  { path: 'not-found', component: PageNotFoundComponent },
+  { path: '**', redirectTo: "/not-found" }
+
+];
 
 @NgModule({
-imports: [RouterModule.forRoot(appRoutes)],
-exports: [RouterModule]
+  imports: [RouterModule.forRoot(appRoutes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule {
 
